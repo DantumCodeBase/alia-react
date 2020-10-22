721200d2baa168932324db62e32957ca17f5cc8e
Moises Diaz <moises.diaz@dantum.mx>
2020-10-22 11:48:37 -0400

    Build release

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,74 +1,117 @@
 import React, { useState, Fragment } from 'react';
 
-var styles = {"test":"_styles-module__test__3ybTi"};
+var styles = {"test":"_3ybTi"};
 
-const Input = () => {
+var Input = function Input() {
   return /*#__PURE__*/React.createElement("input", {
     className: "alia-input"
   });
 };
 
-const Button = ({
-  type: _type = 'button',
-  children
-}) => {
+var Button = function Button(_ref) {
+  var _ref$type = _ref.type,
+      type = _ref$type === void 0 ? 'button' : _ref$type,
+      children = _ref.children;
   return /*#__PURE__*/React.createElement("button", {
-    type: _type,
+    type: type,
     className: "d-button"
   }, children);
 };
 
-const LineChart = props => {
-  const {
-    height,
-    width,
-    padding,
-    data,
-    labels
-  } = props;
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
+var LineChart = function LineChart(props) {
+  var height = props.height,
+      width = props.width,
+      padding = props.padding,
+      data = props.data,
+      labels = props.labels;
   if (!data) return null;
-  const initialState2 = {};
-  data.forEach((data, idx) => initialState2[idx] = true);
-  const [visible, setVisible] = useState(initialState2);
-  const colors = ['#304e62', '#55bcc8', '#ABABAB', '#304e62', '#55bcc8', '#ABABAB'];
-  const FONT_SIZE = 12;
-  const maxX = Math.max(...data.map(d => Math.max(...d.map(e => e.x))));
-  const minX = Math.min(...data.map(d => Math.min(...d.map(e => e.x))));
-  const maxY = Math.max(...data.map(d => Math.max(...d.map(e => e.y)))) * 1.25 || 10;
-  const minY = Math.min(...data.map(d => Math.min(...d.map(e => e.y)))) * 1.25;
+  var initialState2 = {};
+  data.forEach(function (data, idx) {
+    return initialState2[idx] = true;
+  });
+
+  var _useState = useState(initialState2),
+      visible = _useState[0],
+      setVisible = _useState[1];
+
+  var colors = ['#304e62', '#55bcc8', '#ABABAB', '#304e62', '#55bcc8', '#ABABAB'];
+  var FONT_SIZE = 12;
+  var maxX = Math.max.apply(Math, data.map(function (d) {
+    return Math.max.apply(Math, d.map(function (e) {
+      return e.x;
+    }));
+  }));
+  var minX = Math.min.apply(Math, data.map(function (d) {
+    return Math.min.apply(Math, d.map(function (e) {
+      return e.x;
+    }));
+  }));
+  var maxY = Math.max.apply(Math, data.map(function (d) {
+    return Math.max.apply(Math, d.map(function (e) {
+      return e.y;
+    }));
+  })) * 1.25 || 10;
+  var minY = Math.min.apply(Math, data.map(function (d) {
+    return Math.min.apply(Math, d.map(function (e) {
+      return e.y;
+    }));
+  })) * 1.25;
   console.log(minY);
-  const points = data.map(singlePlot => singlePlot.map(element => {
-    const x = (element.x - minX) / (maxX - minX) * width + padding;
-    const y = height - element.y / Math.abs(maxY - minY) * height + padding - Math.abs(minY) / Math.abs(maxY - minY) * height;
-    return `${x},${y}`;
-  }).join(' '));
-
-  const Axis = ({
-    points
-  }) => /*#__PURE__*/React.createElement("polyline", {
-    fill: "solid",
-    stroke: "#EDEDED",
-    strokeWidth: "2",
-    points: points,
-    strokeLinecap: "round"
+  var points = data.map(function (singlePlot) {
+    return singlePlot.map(function (element) {
+      var x = (element.x - minX) / (maxX - minX) * width + padding;
+      var y = height - element.y / Math.abs(maxY - minY) * height + padding - Math.abs(minY) / Math.abs(maxY - minY) * height;
+      return x + "," + y;
+    }).join(' ');
   });
 
-  const XAxis = () => /*#__PURE__*/React.createElement(Axis, {
-    points: `${padding},${height + 5} ${width},${height + 5}`
-  });
+  var Axis = function Axis(_ref) {
+    var points = _ref.points;
+    return /*#__PURE__*/React.createElement("polyline", {
+      fill: "solid",
+      stroke: "#EDEDED",
+      strokeWidth: "2",
+      points: points,
+      strokeLinecap: "round"
+    });
+  };
 
-  const HorizontalGuides = () => {
-    const startX = padding;
-    const endX = width;
-    const numberOfHorizontalGuides = 5;
-    return new Array(numberOfHorizontalGuides).fill(0).map((_, index) => {
-      const yRatio = Math.abs(maxY - minY) / numberOfHorizontalGuides;
-      const ratio = (index + 1) / numberOfHorizontalGuides;
-      const yCoordinate = height - height * ratio + padding;
+  var XAxis = function XAxis() {
+    return /*#__PURE__*/React.createElement(Axis, {
+      points: padding + "," + (height + 5) + " " + width + "," + (height + 5)
+    });
+  };
+
+  var HorizontalGuides = function HorizontalGuides() {
+    var startX = padding;
+    var endX = width;
+    var numberOfHorizontalGuides = 5;
+    return new Array(numberOfHorizontalGuides).fill(0).map(function (_, index) {
+      var yRatio = Math.abs(maxY - minY) / numberOfHorizontalGuides;
+      var ratio = (index + 1) / numberOfHorizontalGuides;
+      var yCoordinate = height - height * ratio + padding;
       return /*#__PURE__*/React.createElement(Fragment, {
         key: index
       }, /*#__PURE__*/React.createElement("text", {
-        key: `${index}-text`,
+        key: index + "-text",
         x: startX,
         y: yCoordinate - 5,
         style: {
@@ -84,22 +127,22 @@ const LineChart = props => {
         strokeDasharray: "5 5",
         strokeLinecap: "round",
         strokeWidth: "1",
-        points: `${startX},${yCoordinate} ${endX},${yCoordinate}`
+        points: startX + "," + yCoordinate + " " + endX + "," + yCoordinate
       }));
     });
   };
 
-  const LabelsXAxis = () => {
-    const y = height - padding + FONT_SIZE * 2 - Math.abs(minY) / Math.abs(maxY - minY) * height;
-    return data[0].map((element, index) => {
-      const x = (element.x - minX) / (maxX - minX) * width + 10 - FONT_SIZE / 2;
+  var LabelsXAxis = function LabelsXAxis() {
+    var y = height - padding + FONT_SIZE * 2 - Math.abs(minY) / Math.abs(maxY - minY) * height;
+    return data[0].map(function (element, index) {
+      var x = (element.x - minX) / (maxX - minX) * width + 10 - FONT_SIZE / 2;
       return /*#__PURE__*/React.createElement("text", {
         key: index,
         x: x,
         y: y,
         dominantBaseline: "central",
         textAnchor: "start",
-        transform: `rotate(45, ${x}, ${y})`,
+        transform: "rotate(45, " + x + ", " + y + ")",
         style: {
           fill: '#ccc',
           fontSize: FONT_SIZE,
@@ -110,10 +153,10 @@ const LineChart = props => {
     });
   };
 
-  const handleVisible = idx => {
-    setVisible(prevState => {
-      const newState = { ...prevState
-      };
+  var handleVisible = function handleVisible(idx) {
+    setVisible(function (prevState) {
+      var newState = _extends({}, prevState);
+
       newState[idx] = !newState[idx];
       return newState;
     });
@@ -122,19 +165,23 @@ const LineChart = props => {
   return /*#__PURE__*/React.createElement(Fragment, null, /*#__PURE__*/React.createElement("div", {
     className: "phase",
     style: {
-      float: 'right',
+      "float": 'right',
       marginBottom: '15px'
     }
-  }, points.map((points, idx) => /*#__PURE__*/React.createElement("a", {
-    key: idx,
-    style: {
-      color: colors[idx],
-      opacity: visible[idx] ? 1 : 0.5
-    },
-    onClick: e => handleVisible(idx)
-  }, labels[idx]))), /*#__PURE__*/React.createElement("svg", {
-    viewBox: `0 0 ${width} ${height + 100}`
-  }, /*#__PURE__*/React.createElement("style", null, `.small {color: 'red'} `), /*#__PURE__*/React.createElement(XAxis, null), /*#__PURE__*/React.createElement(LabelsXAxis, null), /*#__PURE__*/React.createElement(HorizontalGuides, null), points.map((points, idx) => {
+  }, points.map(function (points, idx) {
+    return /*#__PURE__*/React.createElement("a", {
+      key: idx,
+      style: {
+        color: colors[idx],
+        opacity: visible[idx] ? 1 : 0.5
+      },
+      onClick: function onClick(e) {
+        return handleVisible(idx);
+      }
+    }, labels[idx]);
+  })), /*#__PURE__*/React.createElement("svg", {
+    viewBox: "0 0 " + width + " " + (height + 100)
+  }, /*#__PURE__*/React.createElement("style", null, ".small {color: 'red'} "), /*#__PURE__*/React.createElement(XAxis, null), /*#__PURE__*/React.createElement(LabelsXAxis, null), /*#__PURE__*/React.createElement(HorizontalGuides, null), points.map(function (points, idx) {
     if (visible[idx]) {
       return /*#__PURE__*/React.createElement("polyline", {
         key: idx,
@@ -149,63 +196,73 @@ const LineChart = props => {
   })));
 };
 
-const BarChart = props => {
-  const {
-    height,
-    width,
-    padding,
-    data
-  } = props;
-  if (!data) return null;
-  const initialState2 = {};
+var BarChart = function BarChart(props) {
+  var _ref;
 
-  for (const key in data.data) {
+  var height = props.height,
+      width = props.width,
+      padding = props.padding,
+      data = props.data;
+  if (!data) return null;
+  var initialState2 = {};
+
+  for (var key in data.data) {
     initialState2[key] = true;
   }
 
-  const [visible, setVisible] = useState(initialState2);
-  const colors = ['#55BDC8', '#304e62', '#ABABAB'];
-  const FONT_SIZE = 12;
-  const maxValue = Math.max(...[].concat(...Object.values(data.data)));
-  const zeros = 10 ** (maxValue.toFixed().toString().length - 1);
-  let maxY = Math.ceil(maxValue / zeros) * zeros;
+  var _useState = useState(initialState2),
+      visible = _useState[0],
+      setVisible = _useState[1];
+
+  var colors = ['#55BDC8', '#304e62', '#ABABAB'];
+  var FONT_SIZE = 12;
+  var maxValue = Math.max.apply(Math, (_ref = []).concat.apply(_ref, Object.values(data.data)));
+  var zeros = Math.pow(10, maxValue.toFixed().toString().length - 1);
+  var maxY = Math.ceil(maxValue / zeros) * zeros;
 
   if (maxY < 1) {
     maxY = 1;
   }
 
-  const xValues = Math.max(...Object.keys(data.data).map((key, idx) => data.data[key].length));
-  const deltaX = (width - 50) / xValues;
-  const points = Object.values(data.data).map(series => {
-    return series.map((point, idx) => [50 + deltaX * idx + 8, height - point / maxY * height]);
+  var xValues = Math.max.apply(Math, Object.keys(data.data).map(function (key, idx) {
+    return data.data[key].length;
+  }));
+  var deltaX = (width - 50) / xValues;
+  var points = Object.values(data.data).map(function (series) {
+    return series.map(function (point, idx) {
+      return [50 + deltaX * idx + 8, height - point / maxY * height];
+    });
   });
 
-  const Axis = ({
-    points
-  }) => /*#__PURE__*/React.createElement("polyline", {
-    fill: "solid",
-    stroke: "#EDEDED",
-    strokeWidth: "2",
-    points: points,
-    strokeLinecap: "round"
-  });
+  var Axis = function Axis(_ref2) {
+    var points = _ref2.points;
+    return /*#__PURE__*/React.createElement("polyline", {
+      fill: "solid",
+      stroke: "#EDEDED",
+      strokeWidth: "2",
+      points: points,
+      strokeLinecap: "round"
+    });
+  };
 
-  const XAxis = () => /*#__PURE__*/React.createElement(Axis, {
-    points: `${padding},${height + 36} ${width},${height + 36}`
-  });
+  var XAxis = function XAxis() {
+    return /*#__PURE__*/React.createElement(Axis, {
+      points: padding + "," + (height + 36) + " " + width + "," + (height + 36)
+    });
+  };
 
-  const HorizontalGuides = () => {
-    const startX = padding;
-    const endX = width;
-    const numberOfHorizontalGuides = 5;
-    return new Array(numberOfHorizontalGuides).fill(0).map((_, index) => {
-      const yRatio = maxY / numberOfHorizontalGuides;
-      const ratio = (index + 1) / numberOfHorizontalGuides;
-      const yCoordinate = height - height * ratio + padding + 20;
+  var HorizontalGuides = function HorizontalGuides() {
+    var startX = padding;
+    var endX = width;
+    var numberOfHorizontalGuides = 5;
+    return new Array(numberOfHorizontalGuides).fill(0).map(function (_, index) {
+      var yRatio = maxY / numberOfHorizontalGuides;
+      var ratio = (index + 1) / numberOfHorizontalGuides;
+      var yCoordinate = height - height * ratio + padding + 20;
       return /*#__PURE__*/React.createElement(Fragment, {
         key: index
       }, /*#__PURE__*/React.createElement("text", {
-        key: `${index}-text`,
+        key: index + "-text",
         x: startX,
         y: yCoordinate - 5,
         style: {
@@ -220,15 +277,15 @@ const BarChart = props => {
         strokeDasharray: "3 3",
         strokeLinecap: "round",
         strokeWidth: "1",
-        points: `${startX},${yCoordinate} ${endX},${yCoordinate}`
+        points: startX + "," + yCoordinate + " " + endX + "," + yCoordinate
       }));
     });
   };
 
-  const LabelsXAxis = () => {
-    const y = height - padding + FONT_SIZE * 2;
-    return data.labels.map((element, index) => {
-      const x = (index - 0) / (12.7 - 0) * width + 65 - FONT_SIZE / 2;
+  var LabelsXAxis = function LabelsXAxis() {
+    var y = height - padding + FONT_SIZE * 2;
+    return data.labels.map(function (element, index) {
+      var x = (index - 0) / (12.7 - 0) * width + 65 - FONT_SIZE / 2;
       return /*#__PURE__*/React.createElement("text", {
         key: index,
         x: x,
@@ -243,10 +300,10 @@ const BarChart = props => {
     });
   };
 
-  const handleVisible = idx => {
-    setVisible(prevState => {
-      const newState = { ...prevState
-      };
+  var handleVisible = function handleVisible(idx) {
+    setVisible(function (prevState) {
+      var newState = _extends({}, prevState);
+
       newState[idx] = !newState[idx];
       return newState;
     });
@@ -255,21 +312,25 @@ const BarChart = props => {
   return /*#__PURE__*/React.createElement(Fragment, null, /*#__PURE__*/React.createElement("div", {
     className: "phase",
     style: {
-      float: 'right',
+      "float": 'right',
       marginBottom: '15px'
     }
-  }, Object.keys(data.data).map((key, idx) => /*#__PURE__*/React.createElement("a", {
-    key: idx,
-    style: {
-      color: colors[idx],
-      opacity: visible[key] ? 1 : 0.5
-    },
-    onClick: e => handleVisible(key)
-  }, key))), /*#__PURE__*/React.createElement("svg", {
-    viewBox: `0 0 ${width} ${height + 60}`
-  }, /*#__PURE__*/React.createElement("style", null, `.small {color: 'red'} `), /*#__PURE__*/React.createElement(XAxis, null), /*#__PURE__*/React.createElement(LabelsXAxis, null), /*#__PURE__*/React.createElement(HorizontalGuides, null), points.map((series, idx) => {
+  }, Object.keys(data.data).map(function (key, idx) {
+    return /*#__PURE__*/React.createElement("a", {
+      key: idx,
+      style: {
+        color: colors[idx],
+        opacity: visible[key] ? 1 : 0.5
+      },
+      onClick: function onClick(e) {
+        return handleVisible(key);
+      }
+    }, key);
+  })), /*#__PURE__*/React.createElement("svg", {
+    viewBox: "0 0 " + width + " " + (height + 60)
+  }, /*#__PURE__*/React.createElement("style", null, ".small {color: 'red'} "), /*#__PURE__*/React.createElement(XAxis, null), /*#__PURE__*/React.createElement(LabelsXAxis, null), /*#__PURE__*/React.createElement(HorizontalGuides, null), points.map(function (series, idx) {
     if (visible[Object.keys(data.data)[idx]]) {
-      return series.map((point, idxline) => {
+      return series.map(function (point, idxline) {
         return /*#__PURE__*/React.createElement("line", {
           key: idxline,
           x1: point[0] + idx * 14,
@@ -287,9 +348,8 @@ const BarChart = props => {
   })));
 };
 
-const ExampleComponent = ({
-  text
-}) => {
+var ExampleComponent = function ExampleComponent(_ref) {
+  var text = _ref.text;
   return /*#__PURE__*/React.createElement("div", {
     className: styles.test
   }, "Example Component: ", text);
